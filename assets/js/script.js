50018ab1b4dda09c0a6d316bf409a2b21bbd58e8
Shkendi <nshkendi@gmail.com>
2024-01-10 14:12:00 +0000

    fixed how to update the hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     var currentDay = dayjs().format('dddd, MMMM D YYYY')
     $('#currentDay').text(currentDay)
 
-    var currentHour = dayjs().hour()
+    // var currentHour = dayjs().hour()
+    var currentHour = 12
 
     // set up business hours
     var businessHours = 9
@@ -14,12 +15,10 @@ $(document).ready(function () {
 
     $('.container').empty()
 
-    for (var hour = businessHours; hour <= endBusinessHours; hour++) {
-      var $timeBlock = $('<div>').addClass('row time-block')
-      var $hourDiv = $('<div>')
-        .addClass('col-md-1 hour')
-        .text(dayjs(hour, 'H').format('hA'))
-      var $eventInput = $('<textarea>').addClass(
+    for (var hour = 9; hour <= endBusinessHours; hour++) {
+      var timeBlock = $('<div>').addClass('row time-block')
+      var hourDiv = $('<div>').addClass('col-md-1 hour').text(hour)
+      var eventInput = $('<textarea>').addClass(
         'col-md-10 description event-input'
       )
       var $saveBtn = $('<button>')
@@ -27,16 +26,15 @@ $(document).ready(function () {
         .html('<i class="far fa-save"></i>')
 
       // Checking if the hour is past, present, or future and color code acordingly
-
       if (hour < currentHour) {
-        $timeBlock.addClass('past')
+        timeBlock.addClass('past')
       } else if (hour === currentHour) {
-        $timeBlock.addClass('present')
-      } else {
-        $timeBlock.addClass('future')
+        timeBlock.addClass('present')
+      } else if (hour > currentHour) {
+        timeBlock.addClass('future')
       }
-      $timeBlock.append($hourDiv, $eventInput, $saveBtn)
-      $('.container').append($timeBlock)
+      timeBlock.append(hourDiv, eventInput, $saveBtn)
+      $('.container').append(timeBlock)
     }
   }
 
@@ -54,22 +52,25 @@ $(document).ready(function () {
           .replace(/[⌃\d.]/g, '')
       )
 
-      if (blockHour < currentHour) {
-        $(this).removeClass('present future').addClass('past')
-      } else if (blockHour === currentHour) {
-        $(this).removeClass('past future').addClass('present')
-      } else {
-        $(this).removeClass('past present').addClass('future')
-      }
+      // if (blockHour < currentHour) {
+      //   $(this).removeClass('present future').addClass('past')
+      // } else if (blockHour === currentHour) {
+      //   $(this).removeClass('past future').addClass('present')
+      // } else {
+      //   $(this).removeClass('past present').addClass('future')
+      // }
     })
   }
   setInterval(updateCurrentHour(), 60000)
-  updateCurrentHour()
+  // updateCurrentHour()
 
   // Save button click event to store event in local storage
-  $('.container').on('click', '.save-btn', function () {
+  $('.container').on('click', function () {
+    console.log('hit')
     var $eventText = $(this).siblings('.event-input').val()
     var hourIndex = $(this).siblings('.hour').text()
+    console.log(hourIndex)
+
     localStorage.setItem(hourIndex, $eventText)
   })
 
@@ -78,7 +79,6 @@ $(document).ready(function () {
     $('.hour').each(function () {
       var hourIndex = $(this).text()
       var storedEvent = localStorage.getItem(hourIndex)
-
       if (storedEvent !== null) {
         $(this).siblings('event-input').val(storedEvent)
       }
